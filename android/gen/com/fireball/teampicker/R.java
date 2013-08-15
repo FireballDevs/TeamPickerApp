7bca63ad58c64e979e309c89dc176402a75cc1b0
MarkHerhold <Mark.Herhold@gmail.com>
2013-08-14 21:48:10 -0400

    Changing background from black to white

--- android/gen/com/fireball/teampicker/R.java
+++ android/gen/com/fireball/teampicker/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int black=0x7f060003;
         public static final int grey=0x7f060000;
         public static final int light_blue=0x7f060001;
         public static final int white=0x7f060002;
